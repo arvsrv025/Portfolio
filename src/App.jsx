7579ce5f4031275c1b6d23ead7990afd584b43f0
Arnav Srivastava <aranavsrivastav25@gmail.com>
2024-12-29 01:24:34 +0530

    craeted the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
 
 import './index.css'
-import Loading from './Components/Loading'
+import Loading from './Components/Loading/Loading.jsx'
 import Landing from './Components/Landing'
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
-    const timer=setTimeout(()=> setLoading(false),100000);
+    const timer=setTimeout(()=> setLoading(false),6000);
 
     return ()=> clearTimeout(timer);
   },[]);
---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import background from "../../assets/loading-back.jpg"
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className='flex-auto items-center justify-around bg-black'>
+    <div style={{  backgroundImage: `url(${background})` }} className='dekstop bg-no-repeat bg-center flex items-center justify-center h-screen '>
+    <div>
+    <p className="flex-auto items-center justify-center text-center text-[10em] text-slate-300">
+     Y.A.R.N.
+    </p>
+    <br></br>
+      <p className="flex-auto items-center justify-center text-center text-[2em] text-white">
+      Yet Another Resourseful Nerd
+      </p>
+
+    </div>
+    
+    </div>
+    
+    </div>
+    
+  )
+}
+
+export default Loading
